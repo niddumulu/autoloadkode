106c7be9e1233f546c8a1a5c9e26cae93fd9c000
niddumulu <36333121+niddumulu@users.noreply.github.com>
2021-01-17 19:57:27 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+#Telegram Bot API token (ask BotFather):
+botToken = '1598059988:AAEOq4O6P_BoV_UUAQ-6IzXZuRli1Zffl-U'
+
+#Password for authentication in bot:
+botPassword = 'bondowani'
+
+#Default data for ssh-connection:
+sshUser = 'root'
+sshPassword = 'MASulum1a'
+sshHost = 'root'
+sshHomeDirectory = '/'
+
+#Root permission. If 'True" - root allowed
+rootPermission = False
+
+#Logging. If 'True" - logging ON
+logging = True
+logFile = '/var/log/telegramBot/telegramBot.log'
